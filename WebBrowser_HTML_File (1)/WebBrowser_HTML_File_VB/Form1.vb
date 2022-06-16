d42a2d6a0ca4dca9e0b77abba14edc49e1cad6f8
toheed afzaal <toheedafzaal620@gmail.com>
2022-06-16 09:38:28 +0500

    InitialCommit

---
+++ WebBrowser_HTML_File (1)/WebBrowser_HTML_File_VB/Form1.vb
@@ -0,0 +1,11 @@
+﻿
+Imports System.IO
+Imports System.Reflection
+
+Public Class Form1
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim assmbly As Assembly = Assembly.GetExecutingAssembly()
+        Dim reader As New StreamReader(assmbly.GetManifestResourceStream("WebBrowser_HTML_File_VB.HTML.htm"))
+        WebBrowser1.DocumentText = reader.ReadToEnd()
+    End Sub
+End Class
