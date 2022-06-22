34be9d1e29060154700ddfb91deab04917cbf33e
toheed afzaal <toheedafzaal620@gmail.com>
2022-06-22 21:10:46 +0500

    Version 3

--- WebBrowser_HTML_File (1)/WebBrowser_HTML_File_CS/LoginScreen.Designer.cs
+++ WebBrowser_HTML_File (1)/WebBrowser_HTML_File_CS/LoginScreen.Designer.cs
@@ -71,6 +71,7 @@ namespace WebBrowser_HTML_File_CS
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(319, 203);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(195, 26);
             this.textBox2.TabIndex = 3;
             // 
